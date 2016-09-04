7c3283faf06c6c6ff1cad749b1eaf69f42572553
Omar Daniel Leon Alvarado <leon.alvarado12@gmail.com>
2016-09-04 18:10:58 -0500

    Update Versus_PhyML-RAxML.R

--- Versus_PhyML-RAxML.R
+++ Versus_PhyML-RAxML.R
@@ -11,10 +11,12 @@
 
 ## Description
 # This script organize the branch length of each species in both phylogenies (RAxML and PhyML),
-# Then correlates the RAxML branch length against PhyML Branch Length of each species
-# The corrwlation method is pearson
-#
-# The final outcome, is graphic with the Pearson estimate and squared Pearson estimate
+# and for each treatment, Phyml-Optimize, Phyml-NonOptimize and RAxML
+# The final outcome, is data frame, with 4 columns.
+# 1: Species names
+# 2: Phyml-NonOptimize Branch Length
+# 3: Phyml-Optimize Branch Length
+# 4: RAxML Branch Length
 
 ## Load the libraries
 
@@ -24,7 +26,15 @@ library(phytools)
 library(classInt)
 library(ggplot2)
 
-setwd("~/Documentos/Omar/Projects/PhyML-RAxML/PhyML_Trees/")
+#########################################################################################
+#########################################################################################
+#                          NON-OPTIMIZE BRANCH LENGTH PHYML                             #
+#########################################################################################
+#########################################################################################
+
+## First, the Phyml-Nonoptimize trees
+
+setwd("~/Documentos/Omar/Projects/PhyML-RAxML/Comparisons/PhyML-GTR-NonOp_trees")
 
 ## Read phyml trees
 dir.tree <- dir()[grep(".tree",dir())]
@@ -50,20 +60,85 @@ bl <- c()
 
 for (i in 1:length(multi.phylo.phyml)){
   
-  tax <- multi.phylo.phyml[[i]]$tip.label
+  tax <- multi.phylo.phyml[[i]]$tip.label ## Extrac the tip labels or taxa names
   dead.pool <- c(dead.pool,tax) ## Species from phylogenies
+  
+  # Here, each tip label area equivalent to a number from 1 to length of tip labels
+  # Those number will be find in the colum 2 of the edge data
+  
   eg <- which(multi.phylo.phyml[[i]]$edge[,2]%in%1:length(multi.phylo.phyml[[i]]$tip.label))
+  
+  # And given the position in eg, the branch length of each taxa will be extracted
+  
   aa <- multi.phylo.phyml[[i]]$edge.length[eg] # Branch length
+  
+  # And then, assigned to the bl vector
   
   bl <- c(bl,aa) ## Brach length
   
 }
 
+
 ##########################################################################
-#### RAxML
+##########################################################################
+#                   OPTIMIZE BRANCH LENGTH PHYML TREES                   #
+##########################################################################
+##########################################################################
 
 
-setwd("~/Documentos/Omar/Projects/PhyML-RAxML/RAxML_Trees/")
+setwd("~/Documentos/Omar/Projects/PhyML-RAxML/Comparisons/Phyml-GTR-Op_trees/")
+
+## Read phyml trees
+dir.tree <- dir()[grep(".tree",dir())]
+#dir.tree <- dir.tree[-grep("~",dir.tree)]
+
+# Creat a empty list where we will put the trees.
+multi.phylo.phyml2 <- list()
+
+for (i in 1:length(dir.tree)){
+  tree <- read.tree(dir.tree[i]) # Read each tree and...
+  #plot.phylo(tree)
+  multi.phylo.phyml2[[i]] <- tree # Put inside the list, at the enda we create a multiphylo object.
+}
+
+## Assign names to each phylogeny
+names(multi.phylo.phyml2) <- dir.tree
+# Create two empty vectors, one for the species and second for branch length values
+dead.pool.p <- c()
+bl.p <- c()
+
+
+## Extract all species terminals
+
+for (i in 1:length(multi.phylo.phyml2)){
+  
+  tax <- multi.phylo.phyml2[[i]]$tip.label ## Extrac the tip labels or taxa names
+  dead.pool.p <- c(dead.pool.p,tax) ## Species from phylogenies
+  
+  # Here, each tip label area equivalent to a number from 1 to length of tip labels
+  # Those number will be find in the colum 2 of the edge data
+  
+  eg <- which(multi.phylo.phyml2[[i]]$edge[,2]%in%1:length(multi.phylo.phyml2[[i]]$tip.label))
+  
+  # And given the position in eg, the branch length of each taxa will be extracted
+  
+  aa <- multi.phylo.phyml2[[i]]$edge.length[eg] # Branch length
+  
+  # And then, assigned to the bl vector
+  
+  bl.p <- c(bl.p,aa) ## Brach length
+  
+}
+
+
+##########################################################################
+##########################################################################
+#                             RAxML TREES                                #
+##########################################################################
+##########################################################################
+
+
+setwd("~/Documentos/Omar/Projects/PhyML-RAxML/Comparisons/RaxML-GTR_trees/")
 
 ## Read phyml trees
 dir.tree <- dir()[grep(".tree",dir())]
@@ -94,17 +169,26 @@ for (i in 1:length(multi.phylo.raxml)){
   eg <- which(multi.phylo.raxml[[i]]$edge[,2]%in%1:length(multi.phylo.raxml[[i]]$tip.label))
   aa <- multi.phylo.raxml[[i]]$edge.length[eg] #Branch length
   
-  bl.r <- c(bl,aa) #Branch length
+  bl.r <- c(bl.r,aa) #Branch length
   
 }
 
-# Some species names differs
-out <- which((dead.pool%in%dead.pool.r)==F)
+# Check if some species have different names
+
+which((dead.pool%in%dead.pool.r)==F)
+which((dead.pool%in%dead.pool.p)==F)
+which((dead.pool.p%in%dead.pool.r)==F)
+
+
+#############################################################################
+#                          CREATE THE DATA OUTCOME                          #
+#############################################################################
 
 # Create a data frame with all data 
-bl.data <- data.frame(Sp=dead.pool[-out],
-                      BL.phyml=bl[-out],
-                      BL.raxml=rep(0,length(bl[-out]))) # BL for RAxML is empty
+bl.data <- data.frame(Sp=dead.pool,
+                      BL.phyml.NonOp=bl,
+                      BL.phyml.Op=rep(0,length(bl)), # BL for Optimize Phyml is empty
+                      BL.raxml=rep(0,length(bl))) # BL for RAxML is empty
 
 
 bl.data 
@@ -117,58 +201,24 @@ for(i in 1:length(bl.data$Sp)){
   
   bl.data$BL.raxml[i] <- tt
   
+  tt2 <- bl.p[which(dead.pool.p%in%bl.data$Sp[i])]
+  
+  bl.data$BL.phyml.Op[i] <- tt2
+  
 }
 
+## Check the results
 
-## Made the quatiles range for RAxML and PhyML
-brks.p <- classIntervals(bl.data$BL.phyml,n=5,style = "quantile")
-brks.r <- classIntervals(bl.data$BL.raxml,n=5,style = "quantile")
+bl.data
 
-brks.p <- brks.p$brks
-brks.r <- brks.r$brks
 
-brks.p
-brks.r
-## Classification given quantiles intervals.
-Class.Phy <- findInterval(bl.data$BL.phyml,brks.p) # Classify each BL value in brks categories
-Class.Rax <- findInterval(bl.data$BL.raxml,brks.r) # Classify each BL value in brks categories
+########################################################################
+#                         SAVE THE RESULTS                             #
+########################################################################
 
-## Do the Pearson correlation (PhyML vs RAxML)
-pearson <- cor.test(y=bl.data$BL.phyml[which(bl.data$BL.phyml<max(bl.data$BL.phyml))],
-                    x=bl.data$BL.raxml[which(bl.data$BL.raxml<max(bl.data$BL.raxml))])
+setwd("~/Documentos/Omar/Projects/PhyML-RAxML/Comparisons/")
 
-## Pearson's R
-pearson$estimate
-## Pearson's squared R
-(pearson$estimate)^2
+write.table(bl.data, "BL.data-Results", quote = F, col.names = T, row.names = F, sep = ",")
 
-## Make the graphic
-p <- ggplot() + geom_point(aes(x=bl.data$BL.raxml[-which(Class.Rax%in%c(5,6))],
-                               y=bl.data$BL.phyml[-which(Class.Phy%in%c(5,6))]))+
-  #geom_smooth(aes(x=bl.data$BL.raxml[which(bl.data$BL.raxml<max(bl.data$BL.raxml))],
-  #               y=bl.data$BL.phyml[which(bl.data$BL.phyml<max(bl.data$BL.phyml))]))+
-  ylab("PhyML Branch Length")+ xlab("RAxML Branch Length")+
-  geom_abline(intercept=0,slope=pearson$estimate)+
-  geom_label(label=paste("R²",round((pearson$estimate^2),digits=4),sep = ": "),
-             aes(x=0.07,y=0.06),
-             size=12)+
-  geom_label(label=paste("R",round(pearson$estimate,digits=2),sep = ": "),
-             aes(x=0.07,y=0.05),
-             size=12)+
-  theme(axis.text = element_text(size = 12),
-        axis.title.y = element_text(size = 20,),
-        axis.title.x = element_text(size = 20,))
 
-p
-
-# Save the png file
-png(filename = "~/Documentos/Omar/Projects/PhyML-RAxML/June6.png",
-    width = 2800,height=1900,res = 300)
-p
-dev.off()
-
-# Find the species with the highs BL for PhyML and RAxML
-bl.data$Sp[which(bl.data$BL.phyml==max(bl.data$BL.phyml))]
-bl.data$Sp[which(bl.data$BL.raxml==max(bl.data$BL.raxml))]
-
-# Something interesting, the species differs form PhyML to RAxML
+###################################################################################################################
